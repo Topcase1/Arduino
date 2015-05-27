72ee7a492d441af3b5497890da42cf8ca1a4f19a
Luigi Sirito <jimbolimbo4@gmail.com>
2015-05-27 19:41:40 +0200

    added another library for i2c display

--- displayLCD_i2c/displayLCD_i2c.ino
+++ displayLCD_i2c/displayLCD_i2c.ino
@@ -10,15 +10,15 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I
 void setup()
 {
   // Switch on the backlight
-  pinMode ( BACKLIGHT_PIN, OUTPUT );
-  digitalWrite ( BACKLIGHT_PIN, HIGH );
+ // pinMode ( BACKLIGHT_PIN, OUTPUT );
+ // digitalWrite ( BACKLIGHT_PIN, LOW );
   
   lcd.begin(16,2);               // initialize the lcd 
 
   lcd.home ();                   // go home
-  lcd.print("Hello, ARDUINO ");  
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print (" WORLD!  ");      
+  lcd.print("Ciao");  
+  //lcd.setCursor ( 0, 1 );        // go to the next line
+  //lcd.print (" WORLD!  ");      
 }
 
 void loop()
